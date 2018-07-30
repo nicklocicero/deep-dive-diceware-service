2d9aee1c240d42c87afd48379d3a13a3ec711a62
Nick Bennett <nick@nickbenn.com>
2018-07-30 11:48:09 -0600

    Prep for OAuth2 server-side authorization lecture.

--- src/main/java/edu/cnm/deepdive/diceware/DicewareConfiguration.java
+++ src/main/java/edu/cnm/deepdive/diceware/DicewareConfiguration.java
@@ -3,8 +3,10 @@ package edu.cnm.deepdive.diceware;
 import java.security.SecureRandom;
 import java.util.Random;
 import java.util.ResourceBundle;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 
 @Configuration
 public class DicewareConfiguration {
